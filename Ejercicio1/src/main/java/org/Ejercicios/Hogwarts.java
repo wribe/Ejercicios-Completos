86b9561715fb6dd90cde19d99864c83d2b258536
wribe <carmenga24teis@gmail.com>
2025-05-11 10:12:21 +0200

    Terminé el ejercicio 1 (?

--- Ejercicio1/src/main/java/org/Ejercicios/Hogwarts.java
+++ Ejercicio1/src/main/java/org/Ejercicios/Hogwarts.java
@@ -8,7 +8,37 @@ public class Hogwarts {
     private HashMap<String, Profesor> materias;
 
     void engadirAlumnx(Mago mago){
+        int min = 0;
+        int max = 3;
         Casa[] casas = new Casa[] {Casa.GRYFINDOR, Casa.HUFFLEPUFF, Casa.RAVENCLAW, Casa.SLYTHERIN};
-        mago.setCasa(casas[(int) Math.random()]);
+        mago.setCasa(casas[(int) (Math.random() * (max - min) + min)]);
+    }
+
+    void graduar(Mago mago){
+        for (Mago m: alumnxs){
+            if (m.equals(mago)){
+                alumnxs.remove(mago);
+            }
+        }
+    }
+
+    void asignarProfesor(String materia, Profesor profesor){
+        materias.put(materia, profesor);
+    }
+
+    void listarProfesores(){
+        for (String materia: materias.keySet()){
+            System.out.println("La materia " + materia + " la imparte el profesor " + materias.get(materia));
+        }
+    }
+
+    ArrayList<Mago> alumnadoCasa(Casa casa){
+        ArrayList<Mago> magos = new ArrayList<>();
+        for (Mago mago: alumnxs) {
+            if (casa.equals(mago.getCasa())) {
+                magos.add(mago);
+            }
+        }
+        return magos;
     }
 }
