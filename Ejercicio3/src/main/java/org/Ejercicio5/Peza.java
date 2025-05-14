b9e4c1d147760f4c6772affb5c09edb4b61ecf2c
wribe <carmenga24teis@gmail.com>
2025-05-14 17:18:41 +0200

    no entiendo el enunciado dudas en tu dm:(

--- Ejercicio3/src/main/java/org/Ejercicio5/Peza.java
+++ Ejercicio3/src/main/java/org/Ejercicio5/Peza.java
@@ -1,4 +1,9 @@
 package org.Ejercicio5;
 
+import java.util.ArrayList;
+
 public abstract Peza implements Conectable{
+    private Cor cor;
+    private String numeroDeSerie;
+    private ArrayList<Peza>
 }
