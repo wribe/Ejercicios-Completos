d136576a8bc6b63a7a832b7377702376df6f0e09
wribe <carmenga24teis@gmail.com>
2025-05-14 17:42:38 +0200

    CREO QUE YA LO ENTIENDO

--- Ejercicio3/src/main/java/org/Ejercicio5/Peza.java
+++ Ejercicio3/src/main/java/org/Ejercicio5/Peza.java
@@ -1,9 +1,53 @@
 package org.Ejercicio5;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class Peza implements Conectable{
     private Cor cor;
     private String numeroDeSerie;
-    private ArrayList<Peza>
+    private ArrayList<Peza> pezas;
+
+    public Peza(Cor cor, String numeroDeSerie) {
+        this.cor = cor;
+        this.numeroDeSerie = numeroDeSerie;
+        this.pezas = new ArrayList<>();
+    }
+
+    public String getNumeroDeSerie() {
+        return numeroDeSerie;
+    }
+
+    public abstract int getNumeroConectores();
+
+    @Override
+    public boolean conectar(Peza p){
+        if (getNumeroConectores() > pezas.size()){
+            pezas.add(p);
+            return true;
+        }return false;
+    }
+
+    @Override
+    public boolean retirarPeza(String numeroSerie){
+        Iterator<Peza> iterator = pezas.iterator();
+        while (iterator.hasNext()){
+            Peza peza = iterator.next();
+            if (peza.getNumeroDeSerie() == numeroSerie){
+                iterator.remove();
+                return true;
+            }
+        }return false;
+    }
+
+//    public void eliminarViaxe(int codigo) {
+//        Iterator<Viaxe> iterator = viaxes.iterator();
+//        while (iterator.hasNext()) {
+//            Viaxe viaxe = iterator.next();
+//            if (viaxe.getCodigo() == codigo) {
+//                iterator.remove();
+//                return;
+//            }
+//        }
+//    }
 }
