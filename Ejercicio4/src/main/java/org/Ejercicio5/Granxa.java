7d2d9e7bb662c9c89a8112084adda3a34abdb539
wribe <carmenga24teis@gmail.com>
2025-05-16 13:41:32 +0200

    JEJE TERMINADOOOOOO

--- Ejercicio4/src/main/java/org/Ejercicio5/Granxa.java
+++ Ejercicio4/src/main/java/org/Ejercicio5/Granxa.java
@@ -1,6 +1,7 @@
 package org.Ejercicio5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Granxa {
     private String nome;
@@ -60,15 +61,15 @@ public class Granxa {
     public void engadirAnimal(TipoAnimal t){
         for (int i = 0; i < numAnimais.length; i++) {
             if (t.equals(TipoAnimal.PORCOS)&& i == 0){
-                numAnimais[i] = numAnimais[i] - 1;
+                numAnimais[i] += 1;
             } else if (t.equals(TipoAnimal.VACAS) && i == 1){
-                numAnimais[i] = numAnimais[i] -1;
+                numAnimais[i] += 1;
             }else if (t.equals(TipoAnimal.GALIÑAS) && i == 2){
-                numAnimais[i] = numAnimais[i] -1;
+                numAnimais[i] += 1;
             } else if (t.equals(TipoAnimal.COELLOS) && i == 3){
-                numAnimais[i] = numAnimais[i] -1;
+                numAnimais[i] += 1;
             }else if (t.equals(TipoAnimal.OVELLAS) && i == 4){
-                numAnimais[i] = numAnimais[i] -1;
+                numAnimais[i] += 1;
             }
         }
     }
@@ -85,10 +86,49 @@ public class Granxa {
     }
 
     public int getNumeroEmpregados(boolean coidadores){
+        int numEmp = 0;
         if (coidadores){
             for (int i = 0; i < empregados.size(); i++) {
+                if (empregados.get(i) instanceof CoidadorAnimais){
+                    numEmp += 1;
+                }
+            } return numEmp;
+        } else {
+            for (int i = 0; i < empregados.size(); i++) {
+                numEmp += 1;
+            } return numEmp;
+        }
+    }
 
+    @Override
+    public String toString() {
+        String s = "Granxa-->" +
+                "nome='" + nome + "\n" +
+                "Localizacion='" + localizacion + "\n" +
+                "Encargado/a:" + encargado + "\n" +
+                "empregados=" + empregados + "\n" +
+                "numAnimais=";
+
+        int p = 0;
+        int v = 0;
+        int g = 0;
+        int c = 0;
+        int o = 0;
+
+        for (int i = 0; i < numAnimais.length; i++) {
+            if (i == 0){
+                p += numAnimais[i];
+            } else if (i == 1){
+                v += numAnimais[i];
+            } else if (i == 2){
+                g += numAnimais[i];
+            } else  if (i == 3){
+                c += numAnimais[i];
+            } else if (i == 4) {
+                o += numAnimais[i];
             }
         }
+
+        return s + '\n' + "porcos: " + p + "\n" + "vacas: " + v + "\n" + "galiñas: " + g + "\n" + "coellos: " + c + "\n" + "ovellas: " + o;
     }
 }
--- Ejercicio4/src/main/java/org/Ejercicio5/Main.java
+++ Ejercicio4/src/main/java/org/Ejercicio5/Main.java
@@ -2,10 +2,20 @@ package org.Ejercicio5;
 // Exercicio 4 (curso 21-22): Granxa
 
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        int[] i = new int[] {5, 5, 5, 5};
+        LocalDate fecha = LocalDate.of(2000, 02, 27);
+        PersoalServizos ps = new PersoalServizos(001, "nome", fecha, 1100, 'R');
+        PersoalServizos ps2 = new PersoalServizos(000, "nome", fecha, 1100, 'R');
+        Granxa g = new Granxa("nome", "localizacion", ps);
+//        Granxa g1 = new Granxa("nome", "")
+        System.out.println(g);
+        g.engadirAnimal(TipoAnimal.OVELLAS);
+        g.engadirEmpregado(ps2);
+        g.getNumeroEmpregados(false);
+        System.out.println(g);
 
-        System.out.println(i);
     }
 }
